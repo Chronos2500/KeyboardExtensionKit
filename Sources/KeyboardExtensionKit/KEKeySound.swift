7627af3d9d7a26c6ce2636bcbfd8098283880b02
Chronos2500 <kuronosusya@outlook.com>
2025-01-30 22:51:54 +0900

    修正1.0(4)

--- Sources/KeyboardExtensionKit/KEKeySound.swift
+++ Sources/KeyboardExtensionKit/KEKeySound.swift
@@ -8,26 +8,24 @@
 import AudioToolbox
 
 public enum KEKeySound {
-    static func playSystemSound(_ sound: String) {
-        let soundsPath = "/System/Library/Audio/UISounds/\(sound)"
-        let url = NSURL(fileURLWithPath: soundsPath)
-        var soundId: SystemSoundID = 0
-        AudioServicesCreateSystemSoundID(url, &soundId)
-        AudioServicesPlaySystemSound(soundId)
+    static func playSystemSound(_ soundID: SystemSoundID) {
+        Task {
+            await AudioServicesPlaySystemSound(soundID)
+        }
     }
 
     public static func tapDelete() {
         // key_press_delete.caf: 1155
-        playSystemSound("key_press_delete.caf")
+        playSystemSound(1155)
     }
 
     public static func tapModifier() {
         // key_press_modifier.caf: 1156
-        playSystemSound("key_press_modifier.caf")
+        playSystemSound(1156)
     }
 
     public static func tapClick() {
-        // key_press_modifier.caf: 1123
-        playSystemSound("key_press_click.caf")
+        // key_press_modifier.caf: 1104
+        playSystemSound(1104)
     }
 }
